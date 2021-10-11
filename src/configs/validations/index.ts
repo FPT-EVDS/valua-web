b649fd4d9fe5581e97016cabd9101a49a34ffb3c
Huy <nguyenhuuhuy203@gmail.com>
2021-10-12 00:08:17 +0700

    fix: validations index.ts

--- src/configs/validations/index.ts
+++ src/configs/validations/index.ts
@@ -1,6 +1,6 @@
 import accountSchema from './accountSchema';
-import roomSchema from './roomSchema';
 import cameraSchema from './cameraSchema';
+import roomSchema from './roomSchema';
 
 // eslint-disable-next-line import/prefer-default-export
 export { accountSchema, roomSchema, cameraSchema };
