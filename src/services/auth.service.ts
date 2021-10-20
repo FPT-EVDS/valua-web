d58e4f2c97d035dc3ee36ca0f2bd8b23ba182ea7
Ngô Tấn Đức <ngotanduc410@gmail.com>
2021-10-20 12:46:18 +0700

    Update refresh token logic

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,14 +2,13 @@ import { AxiosResponse } from 'axios';
 import LoginDto from 'dtos/login.dto';
 import LoginUser from 'dtos/loginUser.dto';
 import User from 'models/user.model';
-import AppUserDto from 'dtos/appUser.dto';
 
 import axiosClient from './axiosClient';
 
 const authServices = {
   login: (payload: LoginDto): Promise<AxiosResponse<LoginUser>> => {
     const url = '/authentication/login';
-    return axiosClient.post(url, payload);
+    return axiosClient.post(url, { ...payload, skipAuthRefresh: true });
   },
   getUserProfile: (): Promise<AxiosResponse<User>> => {
     const url = '/authentication/profile';
