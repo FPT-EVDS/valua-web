3693e795da0d8ba449b48df1ce03f06076ba1775
Ngô Tấn Đức <ngotanduc410@gmail.com>
2021-10-25 01:24:43 +0700

    Init examinee information chart

---
+++ src/components/ExamineeDetailCard/index.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import DetailSubjectExamineeDto from 'dtos/detailSubjectExaminee';
+import React from 'react';
+
+interface Props {
+  examineeSubject: DetailSubjectExamineeDto;
+}
+
+const ExamineeDetailCard = ({ examineeSubject }: Props) => (
+  <Card sx={{ minWidth: 275 }} elevation={2}>
+    <CardHeader
+      title={
+        <Typography
+          sx={{ fontWeight: 'medium', fontSize: 16 }}
+          variant="h5"
+          gutterBottom
+        >
+          Subject&apos;s information
+        </Typography>
+      }
+    />
+    <Box>
+      <CardContent>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Semester"
+              fullWidth
+              value={examineeSubject.semester.semesterName}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Subject"
+              fullWidth
+              value={examineeSubject.subject.subjectName}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Box>
+  </Card>
+);
+
+export default ExamineeDetailCard;
