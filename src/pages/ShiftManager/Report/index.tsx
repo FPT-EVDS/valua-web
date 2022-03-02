e4e57463159e5acffa540adf6706d14ef978a3a6
Ngô Tấn Đức <ngotanduc410@gmail.com>
2022-03-02 10:29:01 +0700

    Fix exam room name in report

--- src/pages/ShiftManager/Report/index.tsx
+++ src/pages/ShiftManager/Report/index.tsx
@@ -88,7 +88,7 @@ const ReportPage = () => {
       minWidth: 220,
       valueFormatter: (params: GridValueFormatterParams) => {
         const examRoom = params.value as ExamRoom;
-        return examRoom.room.roomName;
+        return examRoom.examRoomName;
       },
     },
     {
